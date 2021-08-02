3f78e3bb908187190bcfbbbf479667a681c414ba
Kim Chae Eun <kce3615@naver.com>
2021-08-02 21:28:11 +0900

    add useTitle

---
+++ useTitle/useTitle.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+// useEffect는 componentDidMount/Unmount/Update 역할 둘다 함
+
+export const useTitle = (initialTitle) => {
+  const [Title, setTitle] = useState(initialTitle);
+
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = Title;
+  };
+  useEffect(updateTitle, [Title]);
+  return setTitle;
+};
